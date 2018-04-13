ecd38ad55964d1d7bd2b2df7f89f0aaec36314fb
Peter Elliott <me@smpete.com>
2018-04-13 17:29:12 -0600

    Adding notification channel so push notifications show up on Oreo and up (#18)

--- app/src/main/java/com/layer/xdk/messenger/PushNotificationReceiver.java
+++ app/src/main/java/com/layer/xdk/messenger/PushNotificationReceiver.java
@@ -1,5 +1,6 @@
 package com.layer.xdk.messenger;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -7,12 +8,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 
-import com.layer.xdk.messenger.util.Log;
-import com.layer.xdk.messenger.util.Util;
 import com.layer.sdk.LayerClient;
 import com.layer.sdk.messaging.Conversation;
 import com.layer.sdk.messaging.Message;
@@ -21,6 +21,8 @@ import com.layer.sdk.query.Predicate;
 import com.layer.sdk.query.Query;
 import com.layer.sdk.query.Queryable;
 import com.layer.sdk.query.SortDescriptor;
+import com.layer.xdk.messenger.util.Log;
+import com.layer.xdk.messenger.util.Util;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -143,12 +145,24 @@ public class PushNotificationReceiver extends BroadcastReceiver {
         private final SharedPreferences mPositions;
         private final SharedPreferences mMessages;
         private final NotificationManager mManager;
+        private final String mChannelId;
 
         public Notifications(Context context) {
             mDisableds = context.getSharedPreferences("notification_disableds", Context.MODE_PRIVATE);
             mPositions = context.getSharedPreferences("notification_positions", Context.MODE_PRIVATE);
             mMessages = context.getSharedPreferences("notification_messages", Context.MODE_PRIVATE);
             mManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            mChannelId = context.getString(R.string.channel_id_messages);
+
+            if (mManager != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                // Create the NotificationChannel
+                CharSequence name = context.getString(R.string.channel_name_messages);
+                String description = context.getString(R.string.channel_description_messages);
+                int importance = NotificationManager.IMPORTANCE_HIGH;
+                NotificationChannel mChannel = new NotificationChannel(mChannelId, name, importance);
+                mChannel.setDescription(description);
+                mManager.createNotificationChannel(mChannel);
+            }
         }
 
         public boolean isEnabled() {
@@ -294,7 +308,7 @@ public class PushNotificationReceiver extends BroadcastReceiver {
 
             // Construct notification
             // TODO: use large icon based on avatars
-            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, mChannelId)
                     .setSmallIcon(R.drawable.notification)
                     .setContentTitle(conversationTitle)
                     .setContentText(collapsedSummary)
@@ -317,7 +331,7 @@ public class PushNotificationReceiver extends BroadcastReceiver {
         }
 
         private void notifyOnContentFailure(Context context, Uri conversationId, Uri messageId, String text) {
-            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, mChannelId)
                     .setSmallIcon(R.drawable.notification)
                     .setContentTitle(context.getString(R.string.push_notification_no_content_title))
                     .setContentText(text)
